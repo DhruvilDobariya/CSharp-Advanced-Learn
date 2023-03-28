6c7a99471ff768d89de8efd3d6caab16e7d52e0a
DhruvilDobariya <190540107051@darshan.ac.in>
2023-03-28 11:03:28 +0530

    Learn lamda expression

---
+++ code/4.Lamda Expression/LamdaExpressionLearn/LamdaExpressionLearn/Program1.cs
@@ -0,0 +1,19 @@
+﻿namespace LamdaExpressionLearn
+{
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            int sumOfSqrt = list.Select(element => element * element).Sum();
+            Console.WriteLine(sumOfSqrt);
+
+            // it's like:
+            // int sumOfSqrt = 0;
+            // foreach (int element in list)
+            // {
+            //    sumOfSqrt += element * element;
+            // }
+        }
+    }
+}
---
+++ code/4.Lamda Expression/LamdaExpressionLearn/LamdaExpressionLearn/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LamdaExpressionLearn
+{
+    public class Program2
+    {
+
+        public static void Main(string[] args)
+        {
+            // Single line
+            Func<string, int, bool> isLengthGreaterThenN = (x, y) => x.Length > y;
+
+            Console.WriteLine(isLengthGreaterThenN("Dhruvil Dobariya", 10));
+
+            // Multi line
+            Action<int, int> sum = (x, y) =>
+            {
+                int ans = x + y;
+                Console.WriteLine($"Ans: {ans}");
+            };
+
+            sum(10, 20);
+        }
+    }
+}
